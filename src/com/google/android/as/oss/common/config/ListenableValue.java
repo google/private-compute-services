83d5c0a8d86a657783270b61cf7d71058160c2ad
PCS Team <spina-team@google.com>
2022-06-08 06:30:27 -0700

    Internal change
    PiperOrigin-RevId: 453666137

--- src/com/google/android/as/oss/common/config/ListenableValue.java
+++ src/com/google/android/as/oss/common/config/ListenableValue.java
@@ -22,6 +22,8 @@ import org.checkerframework.checker.nullness.qual.Nullable;
 
 /** Container that emits an observable signal when the value it stores changes. */
 // Note that T extends Object explicitly to make the null checker not accept @Nullable.
+// See [redacted]
+@SuppressWarnings("ExtendsObject")
 public class ListenableValue<T extends Object> {
   private final CopyOnWriteArrayList<ValueListener<T>> listeners;
 
@@ -34,10 +36,14 @@ public class ListenableValue<T extends Object> {
    * listener will be called for the null -> first value transition). Hence, do not use null in
    * general, this factory method is designed to help delayed initialization only.
    */
+  // See [redacted]
+  @SuppressWarnings("ExtendsObject")
   public static <T extends Object> ListenableValue<T> create() {
     return new ListenableValue<T>(null);
   }
 
+  // See [redacted]
+  @SuppressWarnings("ExtendsObject")
   public static <T extends Object> ListenableValue<T> create(T initialValue) {
     return new ListenableValue<T>(initialValue);
   }
