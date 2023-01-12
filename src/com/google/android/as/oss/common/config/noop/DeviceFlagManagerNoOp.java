15547ad118e66ccf556933173a6a2582429f2d38
Oluwarotimi Adesina <oadesina@google.com>
2023-01-12 08:21:54 -0800

    Add no-op implementation for flag manager.
    PiperOrigin-RevId: 501571059

---
+++ src/com/google/android/as/oss/common/config/noop/DeviceFlagManagerNoOp.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2021 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.android.as.oss.common.config.noop;
+
+import android.os.Binder;
+import com.google.android.as.oss.common.config.AbstractFlagManager;
+import com.google.android.as.oss.common.config.FlagListener;
+import com.google.android.as.oss.common.config.FlagNamespace;
+import com.google.android.as.oss.common.config.Listenable;
+import com.google.android.as.oss.common.config.MulticastListenable;
+import org.checkerframework.checker.nullness.qual.Nullable;
+
+/** No-op implementation of FlagManager. */
+class DeviceFlagManagerNoOp extends AbstractFlagManager {
+
+  private final MulticastListenable<FlagListener> listenable;
+  private final String namespace;
+
+  public static DeviceFlagManagerNoOp create(FlagNamespace namespace) {
+    Binder.clearCallingIdentity();
+    return new DeviceFlagManagerNoOp(namespace);
+  }
+
+  @Override
+  protected @Nullable String getProperty(String name) {
+    return namespace;
+  }
+
+  @Override
+  public Listenable<FlagListener> listenable() {
+    return listenable;
+  }
+
+  private DeviceFlagManagerNoOp(FlagNamespace namespace) {
+    this.namespace = namespace.toString();
+    listenable = MulticastListenable.create();
+  }
+}
