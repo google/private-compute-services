41c5654529ed0ccd47ed28b780cdfbdde5d5ed1a
PCS Team <spina-team@google.com>
2024-04-19 23:48:22 -0700

    Automated Code Change
    PiperOrigin-RevId: 626568966

--- src/com/google/android/as/oss/pd/keys/impl/TinkEncryptionHelper.java
+++ src/com/google/android/as/oss/pd/keys/impl/TinkEncryptionHelper.java
@@ -20,6 +20,7 @@ import com.google.android.as.oss.pd.keys.EncryptionHelper;
 import com.google.crypto.tink.HybridDecrypt;
 import com.google.crypto.tink.HybridEncrypt;
 import com.google.crypto.tink.KeysetHandle;
+import com.google.crypto.tink.RegistryConfiguration;
 import com.google.crypto.tink.TinkProtoKeysetFormat;
 import java.io.IOException;
 import java.security.GeneralSecurityException;
@@ -49,13 +50,14 @@ class TinkEncryptionHelper implements EncryptionHelper {
     if (!hasPrivateKey) {
       throw new GeneralSecurityException("cannot decrypt without a private key");
     }
-    HybridDecrypt decrypt = handle.getPrimitive(HybridDecrypt.class);
+    HybridDecrypt decrypt = handle.getPrimitive(RegistryConfiguration.get(), HybridDecrypt.class);
     return decrypt.decrypt(encryptedData, associatedData);
   }
 
   @Override
   public byte[] encrypt(byte[] plainData, byte[] associatedData) throws GeneralSecurityException {
-    HybridEncrypt encrypt = getPublicKeysetHandle().getPrimitive(HybridEncrypt.class);
+    HybridEncrypt encrypt =
+        getPublicKeysetHandle().getPrimitive(RegistryConfiguration.get(), HybridEncrypt.class);
     return encrypt.encrypt(plainData, associatedData);
   }
 
